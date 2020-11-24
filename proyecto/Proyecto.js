9aadc46ec098f4d5f927d22b868e802197ba5d30
CarlosCallejas <49568035+CarlosCallejas@users.noreply.github.com>
2020-11-24 07:12:40 -0600

    xilofono en primer plano, falta definir material

--- proyecto/Proyecto.js
+++ proyecto/Proyecto.js
@@ -10,21 +10,23 @@ mixer = null;
 
 let box = null
 
-var grupoXilofono = new THREE.Object3D()
-
 
 let duration = 20000; // ms
 let currentTime = Date.now();
 let dancers = [];
-let listener, audioLoader, sound =  null;
+
 let directionalLight = null;
 let spotLight = null;
 let ambientLight = null;
 let mapUrl = "../images/checker_large.gif";
 
 //pista
-//objeto THREE vacio con el cual vamos a añadir todas las pistas a la escena
+//objeto THREE vacio con el cual vamos a aÃ±adir todas las pistas a la escena
 let pistas = new THREE.Object3D;
+
+//xilofono
+let xilofonoG = new THREE.Object3D;
+
 
 let anim1 = "../models/dancer/fbx/Hip Hop Dancing.fbx"
 let anim2 = "../models/dancer/fbx/Breakdance Uprock Var 2.fbx"
@@ -55,7 +57,7 @@ async function loadFBX()
                 child.receiveShadow = true;
             }
         } );
-        //console.log(object.animations);
+        console.log(object.animations);
         dancer = object;
         dancers.push(dancer);
         scene.add( object );
@@ -98,29 +100,25 @@ function animate()
 function run() 
 {
     requestAnimationFrame(function() { run(); });
-   // console.log(camera.position)
+    
     // Render the scene
     renderer.render( scene, camera );
 
-    // Spin the cube for next frame
+    // Animar al bailarin en el centro de la escena
     animate();
-    //intento de hacer que el rectangulo siga la camara
-    //creo que sera mejor con una UI
-    //encontre esto https://github.com/poki/three-ui 
-    //note to self: alch ya me estrese supongo que lo vere despues
-    //box.position.set(camera.position.x,camera.position.y,camera.position.z-100)
-    // console.log("camera: ")
-    // console.log(camera.position)
-    // console.log("box: ")
-    // console.log(box.position)
-    grupoXilofono.position.x=-camera.position.x
-    grupoXilofono.position.y=camera.position.y
+
     // Update the camera controller
     orbitControls.update();
 }
 function setBackgroundMusic(){
-    //Cargar un sonido como background y configurarlo como el Audio Object's buffer
-    //Como es musica para background se reproduce en bucle
+    const listener = new THREE.AudioListener();
+    camera.add( listener );
+
+    // create a global audio source
+    const sound = new THREE.Audio( listener );
+
+    // load a sound and set it as the Audio object's buffer
+    const audioLoader = new THREE.AudioLoader();
     audioLoader.load( '../sounds/background.mp3', function( buffer ) {
         sound.setBuffer( buffer );
         sound.setLoop( true );
@@ -146,17 +144,18 @@ function createScene(canvas) {
 
     // Add  a camera so we can view the scene
     camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
-    camera.position.set(5, 90, 255);
+    camera.position.set(-5, 30, 80);
     scene.add(camera);
+
     orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
-    //  orbitControls.target = new THREE.Vector3(0,20,20);
-    // //aqui limitamos el zoom del orbit controller para que no salga del skybox
-    // orbitControls.maxDistance = 400;
-    // //con esto ponemos que rote automaticamente al rededor del bailarín
-    //     //estaria bueno que con cada click del mouse lo detuvieramos
-    // orbitControls.autoRotate = false;
-    // //con esto quitamos el pan para que la camara no salga del skybox (y por que siento que se ve mas guapo así)
-    // orbitControls.enablePan = false;
+    orbitControls.target = new THREE.Vector3(0,20,0);
+    //aqui limitamos el zoom del orbit controller para que no salga del skybox
+    orbitControls.maxDistance = 400;
+    //con esto ponemos que rote automaticamente al rededor del bailarÃ­n
+        //estaria bueno que con cada click del mouse lo detuvieramos
+    orbitControls.autoRotate = false;
+    //con esto quitamos el pan para que la camara no salga del skybox (y por que siento que se ve mas guapo asÃ­)
+    orbitControls.enablePan = false;
         
     // Create a group to hold all the objects
     root = new THREE.Object3D;
@@ -195,15 +194,6 @@ function createScene(canvas) {
 
     ambientLight = new THREE.AmbientLight ( 0x222222 );
     root.add(ambientLight);
-    //Se incializa un listener para el audio
-    listener = new THREE.AudioListener(); 
-     //Se inicializa el loader para cargar los sonidos
-    audioLoader = new THREE.AudioLoader();
-    //Se inicializa una variable global llamada Sound que controla un sonido
-    sound = new THREE.Audio( listener ); 
-    //Se añade el listener a la camara
-    camera.add( listener ); 
- 
     // Set background music
     setBackgroundMusic();
     // Create the objects
@@ -221,7 +211,7 @@ function createScene(canvas) {
     //crear el skybox
         //El skybox es basicamente un mega cubo que encierra todo asi que por eso es tan grande 
         var skyGeometry = new THREE.CubeGeometry(1000,1000,1000)
-        /*se declaran las texturas y se agregan a una lista con la que se le pondrán las texturas al cubo "skybox"*/
+        /*se declaran las texturas y se agregan a una lista con la que se le pondrÃ¡n las texturas al cubo "skybox"*/
         var skymateriales = [
             //estamos creando 6 texturas basicas(las cuales no van a recibir sombras) y le estamos pasando como imagen las diferentes partes del skybox
             new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("../images/skybox/baseIzqIzq.jpg"),side: THREE.DoubleSide}),
@@ -232,16 +222,16 @@ function createScene(canvas) {
             new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("../images/skybox/baseIzq.jpg"),side: THREE.DoubleSide})
 
         ];
-        //se crea el material del cubo normal y se le añade al cubo para despues agregarlo a la escena y presto
+        //se crea el material del cubo normal y se le aÃ±ade al cubo para despues agregarlo a la escena y presto
         var cubeMaterial = new THREE.MeshFaceMaterial(skymateriales);
         var cube = new THREE.Mesh(skyGeometry,cubeMaterial)
-        scene = new THREE.Scene(); 
+        
         scene.add(cube)
     //fin de crear el skybox
 
 
     //crear las pistas
-        //definimos el tamaño de los lados de cada plano con lado 
+        //definimos el tamaÃ±o de los lados de cada plano con lado 
         let lado = 60
         //desplazamiento se refiere a la cantidad de espacio que se mueve cada plano
         let desplazamiento = lado+(lado/2)
@@ -256,10 +246,10 @@ function createScene(canvas) {
             //aqui se rotan los planos para que queden en horizontal y lo bajamos un poco del 0,0,0 para que se vea mejor
             plane.rotation.x = -Math.PI / 2;
             plane.position.y = -4.02;
-            //aqui le decimos a los planos que puedan recibir sombras del bailarín pero que no mande sombras
+            //aqui le decimos a los planos que puedan recibir sombras del bailarÃ­n pero que no mande sombras
             plane.castShadow = false;
             plane.receiveShadow = true;
-            //añadimos ese mesh al objeto vacio pistas, ya que este es el objeto que agregaremos a la escena y no cada pista por separado
+            //aÃ±adimos ese mesh al objeto vacio pistas, ya que este es el objeto que agregaremos a la escena y no cada pista por separado
             pistas.add(plane);
             /*aqui rehacemos lo mismo de arriba solo para otro plano por debajo de el otro*/
             var planes = new THREE.Mesh( geometry, material );
@@ -272,32 +262,31 @@ function createScene(canvas) {
         }
         group.add( pistas );
     //fin de crear pistas
-    var geometry = new THREE.BoxGeometry( 4,5,10 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    box = new THREE.Mesh( geometry, material );
-    camera.add(box)
-    //grupoXilofono.position.set(0,0,0)
-    // //box.position.set(-4.999999999999999,30,79.99999999999999)
-    // //box.position.set(camera.position.x +5,camera.position.y +5,camera.position.z +5)
-    // //camera.add(grupoXilofono)
-    camera.add(grupoXilofono)
-    // box.position.set(-18,20,50)
-    // box.position.set(0,0,0)
-    // //grupoXilofono.add(box)
-    // camera.add(box)
-    // console.log("grupoXilofono")
-    // console.log(grupoXilofono.position)
-    // console.log("camera")
-    // console.log(camera.position)
-    //  //scene.add(box)
-    //  console.log("caja")
-    //  console.log(box.position)
-    //crear el xilofono
 
+    //crear el xilofono
+    var geometry = new THREE.BoxGeometry( 5,8,10 );
+    var material = new THREE.MeshBasicMaterial( {color: 0xC8C8C8} );
+    var posxbox = -32
+    var scaleybox = .8
+    for(i=0;i<8;i++){
+        posxbox+=7
+        scaleybox+=.2
+        box = new THREE.Mesh( geometry, material );
+        box.position.set(posxbox, -17, -50);
+        box.scale.set(1,scaleybox,1)
+        xilofonoG.add(box)   
+    }
+    xilofonoG.children[0].rotation.set(0,0.4,0)
+    xilofonoG.children[1].rotation.set(0,0.335,0)
+    xilofonoG.children[2].rotation.set(0,0.25,0)    
+    xilofonoG.children[3].rotation.set(0,0.110,0) 
+    xilofonoG.children[4].rotation.set(0,0,0) 
+    xilofonoG.children[5].rotation.set(0,-0.110,0)
+    xilofonoG.children[6].rotation.set(0,-0.25,0)
+    xilofonoG.children[7].rotation.set(0,-0.335,0)
+    camera.add(xilofonoG)
     //fin de crear el xilofono
-    ///aqui termina la creación de objetos    
-    
-    
+    ///aqui termina la creaciÃ³n de objetos    
     
     // Now add the group to our scene
     scene.add( root );
